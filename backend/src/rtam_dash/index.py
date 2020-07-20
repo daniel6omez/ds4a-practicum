427e415cd63d2b402ccfc41fbb047e141b55694e
daniel6omez <daniel@celerik.com>
2020-07-20 11:26:19 -0500

    Added spinner

--- backend/src/rtam_dash/index.py
+++ backend/src/rtam_dash/index.py
@@ -4,14 +4,10 @@ from dash.dependencies import Input, Output
 
 from app import app, server
 
-
 from layouts import main_layout
 import callbacks
 
 app.layout = main_layout
 
-
 if __name__ == "__main__":
     app.run_server()
-    #use the bottom option when debbuging
-    #app.run_server(host='localhost',port='8050',debug=True,use_reloader=True)
--- backend/src/rtam_dash/layouts.py
+++ backend/src/rtam_dash/layouts.py
@@ -6,20 +6,23 @@ from modules import md_map, heat_maps
 
 main_layout =html.Div(
 [ 
-    dbc.Row([
-        dbc.Col(
-            dcc.Graph(figure=md_map.update_map(), id='md_map'),xl=6, lg=6, md=6, xs=12
-        ),
-        dbc.Col(
-            dcc.Graph(figure=heat_maps.update_line_graph(),id='line_graph'),xl=6, lg=6, md=6, xs=12
-        )
-    ]),
-    dbc.Row([
-        dbc.Col(
-            dcc.Graph(figure=heat_maps.update_month_day_heat(), id='month_day_heat'),xl=6, lg=6, md=6, xs=12
-        ),
-        dbc.Col(
-            dcc.Graph(figure=heat_maps.update_day_hour_heat(), id='day_hour_heat'),xl=6, lg=6, md=6, xs=12
+    dcc.Loading(
+    [
+        dbc.Row([
+            dbc.Col(
+                dcc.Graph(figure=md_map.update_map(), id='md_map'),xl=6, lg=6, md=6, xs=12
+            ),
+            dbc.Col(
+                dcc.Graph(figure=heat_maps.update_line_graph(),id='line_graph'),xl=6, lg=6, md=6, xs=12
             )
-    ])
+        ]),
+        dbc.Row([
+            dbc.Col(
+                dcc.Graph(figure=heat_maps.update_month_day_heat(), id='month_day_heat'),xl=6, lg=6, md=6, xs=12
+            ),
+            dbc.Col(
+                dcc.Graph(figure=heat_maps.update_day_hour_heat(), id='day_hour_heat'),xl=6, lg=6, md=6, xs=12
+                )
+        ])
+    ],fullscreen=True)
 ])
