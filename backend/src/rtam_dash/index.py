f89ef968d550d753c2fd8367e1dbb2f2e04330e2
daniel6omez <daniel@celerik.com>
2020-07-20 03:45:52 -0500

    Added crossfiltering to the heatmaps

--- backend/src/rtam_dash/index.py
+++ backend/src/rtam_dash/index.py
@@ -36,6 +36,8 @@ x1 = 'xaxis.range[1]'
 
 months={"Jan":1, "Feb":2, "Mar":3, "Apr":4, "May":5, "June":6, "July":7, "Aug":8, "Sept":9, "Oct":10, "Nov":11, "Dec":12}
 days={"Mon":0,"Tue":1,"Wed":2,"Thu":3,"Fri":4,"Sat":5,"Sun":6}
+order_days = {'0Monday':0, '1Tuesday':1, '2Wednesday':2, '3Thursday':3, '4Friday':4, '5Saturday':5, '6Sunday':6}
+    
 
 #PLACE THE COMPONENTS IN THE LAYOUT
 app.layout =html.Div(
@@ -43,7 +45,7 @@ app.layout =html.Div(
       md_map.map,
       heat_maps.heatmaps
     ],
-    className="ds4a-app", #You can also add your own css files by locating them into the assets folder
+    className="ds4a-body", #You can also add your own css files by locating them into the assets folder
 )
 
 
@@ -68,17 +70,18 @@ app.layout =html.Div(
         Input('md_map', 'selectedData'),
         Input('line_graph', 'selectedData'),
         Input('line_graph', 'relayoutData'),
-        Input("month_day_heat", "clickData")
+        Input("month_day_heat", "clickData"),
+        Input("day_hour_heat", "clickData")
     ]
 )
-def update_dashboard(point, points, selected_data, relayout_data, month_day_cd):
+def update_dashboard(point, points, selected_data, relayout_data, month_cd, day_cd):
     global dff
     ctx = dash.callback_context
     id = ''
     if ctx.triggered:
         id = ctx.triggered[0]['prop_id'].split('.')[0]
 
-    if point is None and points is None and selected_data is None and (relayout_data is None or x0 not in relayout_data) and month_day_cd is None:
+    if point is None and points is None and selected_data is None and (relayout_data is None or x0 not in relayout_data) and month_cd is None and day_cd is None:
        raise dash.exceptions.PreventUpdate
 
     if id == 'md_map' and points != None and 'points' in points:
@@ -94,10 +97,16 @@ def update_dashboard(point, points, selected_data, relayout_data, month_day_cd):
         dff=dff[(dff.Date >= relayout_data[x0]) & (dff.Date <= relayout_data[x1])]
     elif id == 'line_graph' and selected_data != None and 'range' in selected_data and 'x' in selected_data['range']:
         dff=dff[(dff.Date >= selected_data['range']['x'][0]) & (dff.Date <= selected_data['range']['x'][1])]
-    elif id == 'month_day_heat' and month_day_cd != None and 'points' in month_day_cd and len(month_day_cd['points']) == 1 and 'x' in month_day_cd['points'][0] and 'y' in month_day_cd['points'][0]:
-        month = months[month_day_cd['points'][0]['x']]
-        day = months[month_day_cd['points'][0]['y']]
-        dff = dff[(dff['Date'].dt.month == month) & (dff['Date'].dt.dayofweek == day)]
+    elif id == 'month_day_heat' and month_cd != None and 'points' in month_cd and len(month_cd['points']) == 1 and 'x' in month_cd['points'][0] and 'y' in month_cd['points'][0]:
+        month = months[month_cd['points'][0]['x']]
+        if dff[(dff['Date'].dt.month != month)].empty:
+            dff = dff[(dff['Date'].dt.month.isin(months.values))]
+        dff = dff[(dff['Date'].dt.month == month)]
+    elif id == 'day_hour_heat' and day_cd != None and 'points' in day_cd and len(day_cd['points']) == 1 and 'x' in day_cd['points'][0] and 'y' in day_cd['points'][0]:
+        dayofweek = order_days[day_cd['points'][0]['x']]
+        if dff[(dff['Date'].dt.dayofweek != dayofweek)].empty:
+            dff = dff[(dff['Date'].dt.month.isin(order_days.values))]
+        dff = dff[(dff['Date'].dt.dayofweek == dayofweek)]
     else :
         dff = df    
 
